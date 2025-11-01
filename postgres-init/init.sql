bef30311eebeb67feaa12746b135fc90d9c8be27
asperes <asperesgh@gmail.com>
2025-11-01 13:36:16 +0000

    Fix: add postgis

---
+++ postgres-init/init.sql
@@ -0,0 +1,5 @@
+-- Enable UUID extension for generating UUIDs
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Enable PostGIS extension for geospatial features
+CREATE EXTENSION IF NOT EXISTS "postgis";
